0c9b290a872774b8a5d0b61ac1617333125acf2b
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-03-07 00:54:42 +0000

    add spec

--- spec/introduction_spec.js
+++ spec/introduction_spec.js
@@ -1,9 +1,9 @@
-import { hello } from '../functions.js'
+import { calculateCoins } from '../valueofcoins.js'
 
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-      let sHello = hello();
-      expect(sHello).toBe("hello");
+describe("calculateCoins", function() {
+    it("tests the calculation with 1 nickle, 2 dimes, 3 quarters, 4 loonies, and 5 toonies", function() {
+      let total = calculateCoins(1,2,3,4,5);
+      expect(total).toBe("15.00");
     });
   });
   
---
+++ spec/valueofcoin_spec.js
@@ -0,0 +1,20 @@
+import { calculateCoins } from "../valueofcoins.js";
+
+describe("calculateCoins", function() {
+    it("tests the calculation with 1 nickle, 2 dimes, 3 quarters, 4 loonies, and 5 toonies", function() {
+      let total = calculateCoins(1,2,3,4,5);
+      expect(total).toBe("15.00");
+    });
+
+    it("tests the calculation with 1 nickle, 1 dime, 1 quarter, 1 loonie, and 1 toonie", function() {
+        let total = calculateCoins(1,1,1,1,1);
+        expect(total).toBe("3.4");
+      });
+
+      it("tests the calculation with 1 nickle, 2 dimes, 3 quarters, 4 loonies, and 5 toonies", function() {
+        let total = calculateCoins(1,2,3,4,5);
+        expect(total).toBe("15");
+      });
+  });
+  
+  
