4c80b718dadd207d521c9faf61638a663d342bbc
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-03-07 01:57:55 +0000

    write membership function

---
+++ gymmembership.js
@@ -0,0 +1,31 @@
+/*Write a function that calculates total cost of a gym membership. A member gets a
+discount according to the number of friends they sign up.
+0 friends: 0% discount
+1 friend: 5% discount
+2 friends: 10% discount
+3 or more friends: 15% discount
+-------------------------------------------------------
+Parameters:
+cost - a gym membership base cost (float > 0)
+friends - number of friends signed up (int >= 0)*/
+
+function calculateMembership(cost, friends) {
+    let total;
+
+    if (friends == 0) {
+        total = cost;
+
+    } else if (friends == 1) {
+        total = cost - (cost * 0.05);
+
+    } else if (friends == 2) {
+        total = cost - (cost * 0.10);
+
+    } else if (friends >= 3) {
+        total = cost - (cost * 0.15);
+    }
+    return total;
+}
+
+console.log(`If the membership cost $10, and I have 2 friends sign up, I pay $${calculateMembership(10,2)} for my membership.`);
+export { calculateMembership }
